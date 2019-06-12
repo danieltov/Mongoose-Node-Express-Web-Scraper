88786f73da4a62e3d6296c11b1e549c28ecdf08d
Daniel Tovar <daniel@danieltovar.io>
2019-06-11 18:29:55 -0600

    Review model made

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 // Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new NoteSchema object
+// Using the Schema constructor, create a new CommentSchema object
 // This is similar to a Sequelize model
 var CommentSchema = new Schema({
   // `title` is of type String
@@ -15,5 +15,5 @@ var CommentSchema = new Schema({
 // This creates our model from the above schema, using mongoose's model method
 var Comment = mongoose.model("Comment", CommentSchema);
 
-// Export the Note model
-module.exports = Note;
+// Export the Comment model
+module.exports = Comment;
