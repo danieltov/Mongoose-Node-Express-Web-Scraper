7b1fbca3d3f1e4c3d630d2cc627a68096c8b2890
Daniel Tovar <daniel@danieltovar.io>
2019-06-15 14:38:05 -0600

    Rudimentary frontend finished, but ugly

--- public/app.js
+++ public/app.js
@@ -1,57 +1,66 @@
 // Grab the reviews as a json
-$.getJSON("/reviews", function (data) {
+$.getJSON('/api/reviews', function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#reviews").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $('#reviews').append(
+      "<p data-id='" +
+        data[i]._id +
+        "'>" +
+        data[i].title +
+        '<br />' +
+        data[i].link +
+        '</p>'
+    );
   }
 });
 
-
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function () {
+$(document).on('click', 'p', function() {
   // Empty the comments from the comment section
-  $("#comments").empty();
+  $('#commentForm').empty();
+  $('#comments').empty();
   // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
+  var thisId = $(this).attr('data-id');
 
   // Now make an ajax call for the review
   $.ajax({
-    method: "GET",
-    url: "/reviews/" + thisId
+    method: 'GET',
+    url: 'api/reviews/' + thisId
   })
     // With that done, add the comment information to the page
-    .then(function (data) {
-      console.log('this is the comment data?');
-      console.log(data);
-      // The title of the review
-      $("#comments").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#comments").append("<input id='nameinput' name='name' >");
-      // A textarea to add a new comment body
-      $("#comments").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new comment, with the id of the review saved to it
-      $("#comments").append("<button data-id='" + data._id + "' id='savecomment'>Save comment</button>");
+    .then(function(data) {
+      const existingComments = data.comments;
+      $('#comments').append(
+        existingComments.map(comment => {
+          return `<h4> ${comment.name}</h4><h5> ${comment.body}</h5>`;
+        })
+      );
 
-      // If there's a comment in the review
-      if (data.comment) {
-        // Place the title of the comment in the title input
-        $("#nameinput").val(data.comment.name);
-        // Place the body of the comment in the body textarea
-        $("#bodyinput").val(data.comment.body);
-      }
+      // The title of the review
+      $('#comments').append('<h2>' + data.title + '</h2>');
+      // An input to enter a new title
+      $('#comments').append("<input id='nameinput' name='name' >");
+      // A textarea to add a new comment body
+      $('#comments').append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new comment, with the id of the review saved to it
+      $('#comments').append(
+        "<button data-id='" +
+          data._id +
+          "' id='savecomment'>Save comment</button>"
+      );
     });
 });
 
 // When you click the savecomment button
-$(document).on("click", "#savecomment", function () {
+$(document).on('click', '#savecomment', function() {
   // Grab the id associated with the review from the submit button
-  var thisId = $(this).attr("data-id");
+  var thisId = $(this).attr('data-id');
   var dataObj = {
     // Value taken from title input
-    name: $("#nameinput").val(),
+    name: $('#nameinput').val(),
     // Value taken from comment textarea
-    body: $("#bodyinput").val()
+    body: $('#bodyinput').val()
   };
 
   console.log(thisId);
@@ -59,19 +68,19 @@ $(document).on("click", "#savecomment", function () {
 
   // Run a POST request to change the comment, using what's entered in the inputs
   $.ajax({
-    method: "POST",
-    url: "/reviews/" + thisId,
+    method: 'POST',
+    url: 'api/reviews/' + thisId,
     data: dataObj
   })
     // With that done
-    .then(function (data) {
+    .then(function(data) {
       // Log the response
       console.log(data);
       // Empty the comments section
-      $("#reviews").empty();
+      $('#comments').empty();
     });
 
   // Also, remove the values entered in the input and textarea for comment entry
-  $("#nameinput").val("");
-  $("#bodyinput").val("");
+  $('#nameinput').val('');
+  $('#bodyinput').val('');
 });
